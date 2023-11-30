8a05880c9bf5cca60eb645ee7efe18090b4cb56f
NecheU <uzorgodsboy1@gmail.com>
2023-11-30 10:57:42 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-#importing funtion add with from
-#to perforn addition operation of two numbers
+# importing funtion add with from
+# to perforn addition operation of two numbers
 
 if __name__ == "__main__":
     from add_0 import add
