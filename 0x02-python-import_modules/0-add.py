a3351f620f7fe4e7ad86f3b8c9c1faf550247be3
Uzor <uzorgodsboy1@gmail.com>
2024-03-14 12:17:37 -0700

    103-fast_alphabet.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+__import__("add_0")
+a = 1
+b = 2
+sum = a + b
+print(sum = add(a, b))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My add function
+
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The value of a + b
+    """
+
+    return (a + b)
