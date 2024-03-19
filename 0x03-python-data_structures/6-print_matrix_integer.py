51a74c8726e327f8bb26a685db319c7383ce5095
Uzor <uzorgodsboy1@gmail.com>
2024-03-19 12:02:48 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for y in row:
-            print("{:d}".format(matrix[row][y], end=" ")
+            print("{:d}".format(matrix[row][y]), end=" ")
         print()
