d6d4d3bb0bc6ac42debe8062e93dc4586e63752d
Uzor <uzorgodsboy1@gmail.com>
2024-03-20 16:35:45 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for h in range(len(matrix)):
         new_row = []
         for k in range(len(matrix[h])):
-            new_row.append(matrix[h][k]** 2)
+            new_row.append(matrix[h][k] ** 2)
         new_mat.append(new_mat)
     return new_mat
