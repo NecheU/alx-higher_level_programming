7ca9f0c77ba2e5ebb3f14f0ce7d0b6815eaa9bbb
Uzor <uzorgodsboy1@gmail.com>
2024-04-02 13:08:23 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     num_of_elem = 0
     try:
         while True:
-            print(my_list[num_of_elem], end=' ')
+            print(my_list[num_of_elem], end='')
             num_of_elem += 1
             if num_of_elem == x:
                 break
