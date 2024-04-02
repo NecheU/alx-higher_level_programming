53d6e41a5e42c97450c2f356097ca17e6461366d
Uzor <uzorgodsboy1@gmail.com>
2024-04-02 12:56:08 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num_of_elem = 0
+    try:
+        while True:
+            print(my_list[num_of_elem], end=' ')
+            num_of_elem += 1
+            if num_of_elem == x:
+                break
+    except IndexError:
+        pass
+    print()
+    return num_of_elem
