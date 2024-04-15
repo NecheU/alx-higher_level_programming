31493dddfb1d184e1d7a69a89e7d449e56f883f1
Uzor <uzorgodsboy1@gmail.com>
2024-04-15 13:09:45 -0700

    101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -2,11 +2,11 @@
 
 """Python program to add a new attribute to an object
 Function: def add_attribute(obj, name, value)
-Args: 
+Args:
     obj: Object that a new attribute will be addes to
     name: name of the attribute
     vakue: Vakue of the attribute
-Raise: 
+Raise:
     TypeError: If new attribute cant be added to rhe object
 """
 
