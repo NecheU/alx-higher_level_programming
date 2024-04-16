186cbb2332c2574de1841739c6c708670d6cc96d
Uzor <uzorgodsboy1@gmail.com>
2024-04-16 05:56:51 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+"""Python Input and Output
+Python program to read text from a file
+"""
+
+
 def read_file(filename=""):
+    """Python program to open and read from a file"""
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
