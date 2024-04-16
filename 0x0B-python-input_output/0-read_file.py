62150f78d7aa459388f0b12bb78ecbfed7a683ad
Uzor <uzorgodsboy1@gmail.com>
2024-04-16 05:51:21 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as f
-    for line in file:
-        print(line, end='')
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
